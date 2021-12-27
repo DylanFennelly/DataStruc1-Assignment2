75a36e56de6be2451b0d996d8b7297869f9e7588
DylanFennelly <20093427@mail.wit.ie>
2021-12-27 19:54:45 +0000

    Lot View tableview
    Added tableview functionality to Lot View

--- bidalot/src/main/java/com/bid/bidalot/controllers/LotController.java
+++ bidalot/src/main/java/com/bid/bidalot/controllers/LotController.java
@@ -1,19 +1,24 @@
 package com.bid.bidalot.controllers;
 
 import com.bid.bidalot.AuctionApp;
+import com.bid.bidalot.objects.Lot;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class LotController {
+    @FXML
+    private TableView<Lot> activeLotsTV;
 
     @FXML
     protected void changeToStartMenu(ActionEvent actionEvent) throws IOException {
@@ -24,7 +29,7 @@ public class LotController {
     }
 
     @FXML
-    protected void changeToAddMenu(ActionEvent actionEvent) throws IOException {    //pops up in new window
+    protected void changeToAddMenu(ActionEvent actionEvent) throws IOException {    //pops up in new window     todo: check for signed in
         Parent addView = FXMLLoader.load(Objects.requireNonNull(AuctionApp.class.getResource("add-view.fxml")));
         Scene addScene = new Scene(addView);
         Stage stage = new Stage();
@@ -33,5 +38,12 @@ public class LotController {
         stage.setScene(addScene);
         stage.setTitle("Add Lot");
         stage.show();
+    }
+
+    @FXML
+    protected void addDummyValues(ActionEvent actionEvent){
+        LocalDate origin = LocalDate.now().minusYears(20);
+        Lot testLot = new Lot("Old chair for sale","Dark oak chair from roughly 20 years ago. Ships from Kilkenny, Ireland.","Furniture","",origin,50.00);
+        activeLotsTV.getItems().add(testLot);
     }
 }
