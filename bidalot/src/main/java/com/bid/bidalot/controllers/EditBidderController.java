03feb7e06704034769d043941ef4e31507a12c88
DylanFennelly <20093427@mail.wit.ie>
2022-01-05 12:43:38 +0000

    Initialisation in EditBidderController
    - Fields are now initialised with bidder details

--- bidalot/src/main/java/com/bid/bidalot/controllers/EditBidderController.java
+++ bidalot/src/main/java/com/bid/bidalot/controllers/EditBidderController.java
@@ -3,11 +3,25 @@ package com.bid.bidalot.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import static com.bid.bidalot.AuctionApp.DRIVER;
+import static com.bid.bidalot.controllers.BidderController.BidderIndex;
 
 public class EditBidderController {
     @FXML
     private Button closeButton;
+    @FXML
+    private TextField BName, BAddress, BPhone;
+
+    @FXML
+    protected void initialize() {
+        //autofilling Bidder deatils into fields
+        BName.setText(DRIVER.bidderList.getElementByInt(BidderIndex).getContents().getName());
+        BAddress.setText(DRIVER.bidderList.getElementByInt(BidderIndex).getContents().getAddress());
+        BPhone.setText(DRIVER.bidderList.getElementByInt(BidderIndex).getContents().getPhone());
+    }
 
 
     protected void closeWindow(){    //https://stackoverflow.com/questions/25037724/how-to-close-a-java-window-with-a-button-click-javafx-project/41838183
